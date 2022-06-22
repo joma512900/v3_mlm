8988eda305f41ca8ef61ccfb23c997bf640514b6
Mr-DecodeBlock <decodeblock1010@gmail.com>
2022-06-22 17:15:56 -0500

    main

--- contracts/infinitySystemV2/UnilevelSystemV2.sol
+++ contracts/infinitySystemV2/UnilevelSystemV2.sol
@@ -9,29 +9,97 @@ interface TRC20_Interface {
   function decimals() external view returns(uint);
 }
 library SafeMath {
-  function mul(uint a, uint b) internal pure returns (uint) {
-    if (a == 0) {
-        return 0;
+   function tryAdd(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            uint256 c = a + b;
+            if (c < a) return (false, 0);
+            return (true, c);
+        }
     }
-    uint c = a * b;
-    require(c / a == b);
-    return c;
-  }
-  function div(uint a, uint b) internal pure returns (uint) {
-    require(b > 0);
-    uint c = a / b;
-    return c;
-  }
-  function sub(uint a, uint b) internal pure returns (uint) {
-    require(b <= a);
-    uint c = a - b;
-    return c;
-  }
-  function add(uint a, uint b) internal pure returns (uint) {
-    uint c = a + b;
-    require(c >= a);
-    return c;
-  }
+    
+    function trySub(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b > a) return (false, 0);
+            return (true, a - b);
+        }
+    }
+
+    function tryMul(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+          
+            if (a == 0) return (true, 0);
+            uint256 c = a * b;
+            if (c / a != b) return (false, 0);
+            return (true, c);
+        }
+    }
+  
+    function tryDiv(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a / b);
+        }
+    }
+
+    function tryMod(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a % b);
+        }
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a + b;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a - b;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a * b;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a / b;
+    }
+
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a % b;
+    }
+
+    function sub(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b <= a, errorMessage);
+            return a - b;
+        }
+    }
+
+    function div(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a / b;
+        }
+    }
+
+    function mod(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a % b;
+        }
+    }
 }
 contract Admin {
   address payable public owner;
